983f0618054163151587cc00867f2707bcd0281b
Alexander Nestorov <trailer93f7123@gmail.com>
2023-03-02 00:49:41 +0200

    Administration pannel layout and index page, Client Action filter, File server routing and index view

---
+++ Client/ActionFilters/UsersAuthenticationFilter.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Client.ActionFilters
+{
+	public class UsersAuthenticationFilter : ActionFilterAttribute
+	{
+		public override void OnActionExecuting(ActionExecutingContext context)
+		{
+			if (string.IsNullOrEmpty(context.HttpContext.Session.GetString("UserId")))
+			{
+				context.Result = new RedirectResult("/Authentication/Login");
+			}
+		}
+	}
+}
--- Client/Controllers/HomeController.cs
+++ Client/Controllers/HomeController.cs
@@ -7,14 +7,26 @@ namespace Client.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+		private IConfiguration _configuration;
 
-        public HomeController(ILogger<HomeController> logger)
+		private readonly string _FreelanceURI;
+		private readonly string _UsersURI;
+		private readonly string _FileServerURI;
+
+		public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
         {
             _logger = logger;
-        }
+            _configuration = configuration;
+
+			_FreelanceURI = _configuration.GetValue<string>("FrelanceAPI");
+			_UsersURI = _configuration.GetValue<string>("UsersAPI");
+			_FileServerURI = _configuration.GetValue<string>("FileServerAPI");
+		}
 
         public IActionResult Index()
         {
+            ViewData["BaseImagesPath"] = _FileServerURI + "/Freelance/BaseImages/";
+
             return View();
         }
 
